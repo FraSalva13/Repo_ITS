53b7e5dbf015f46b2e0f2081c534e6596856e760
FraSalva13 <francesca.salvarezza.14-2@accademiadigitaleliguria.it>
2025-06-25 15:32:27 +0200

    #1 modifica

---
+++ progetto/ConsoleApp1/CalcolatriceLib/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcolatriceLib
+{
+    public class Calcolatrice
+    {
+        public Calcolatrice()
+        {
+        }
+
+        public double Somma(double x, double y)
+        {
+            //throw new NotImplementedException();
+           // return 5;
+           return x + y;
+        }
+
+    }
+}
---
+++ progetto/ConsoleApp1/TestCalcolatrice/Test1.cs
@@ -0,0 +1,25 @@
+﻿using CalcolatriceLib;
+namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            //crea test della somma
+            CalcolatriceLib.Calcolatrice calcolatrice = new CalcolatriceLib.Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato, "La somma di 2 e 3 dovrebbe essere 5");
+        }
+        [TestMethod]
+
+        public void TestSommaConZero()
+        {
+            //crea test della somma con zero
+            Calcolatrice calcolatrice = new Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 0);
+            Assert.AreEqual(2, risultato, "La somma di 2 e 0 dovrebbe essere 2");
+        }
+    }
+}
